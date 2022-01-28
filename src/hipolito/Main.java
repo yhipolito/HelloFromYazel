b95469add87cfb4992b1d9dd8f0476a5ccf84655
eduardoserrano <ser20003@byui.edu>
2022-01-27 21:46:43 -0600

    Added new line of code. Eduardo

--- src/hipolito/Main.java
+++ src/hipolito/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Yazel");
         System.out.println("Hello Yazel, my name is Andres");
+        System.out.println("Hello, Yazael and Andres. My name is Eduardo");
     }
 }
